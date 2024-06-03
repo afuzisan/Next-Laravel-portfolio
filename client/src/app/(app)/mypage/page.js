4f3773a4b5eb4847265354cfce27d261e269599d
akiyaoka <akiyaoka@gmail.com>
2024-06-03 19:23:04 +0900

    ffff

--- client/src/app/(app)/mypage/page.js
+++ client/src/app/(app)/mypage/page.js
@@ -13,8 +13,8 @@ const Dashboard = () => {
                     <div>投資系サイトへのリンク編集</div>
                     <div>１ページに表示する銘柄数の編集</div>
                     <div>パスワードリセットへのボタンリンク</div>
-                    <div>アカウント削除</div>
-                    <div>チャート表示切り替え編集aaa</div>
+                    <div>アカウント削除ff</div>
+                    <div>チャート表示切り替え編集</div>
                 </div>
                 <div className="grid justify-center bg-white p-6">
                     <div>
