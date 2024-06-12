96ccf35d24efdda0d3687e47a2196803e09bae91
akiyaoka <akiyaoka@gmail.com>
2024-06-13 03:36:38 +0900

    中間テーブルを作った

---
+++ server/database/migrations/2024_06_12_182228_stocks_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('stock_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('stock_id')->references('id')->on('stocks')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->primary(['stock_id', 'user_id']);  // 複合主キー
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks_users');
+    }
+};
