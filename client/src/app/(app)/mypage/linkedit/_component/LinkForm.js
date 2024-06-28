aa01635d4ea135e7ae8bd2af8002252f7d23f906
akiyaoka <akiyaoka@gmail.com>
2024-06-28 17:01:22 +0900

    postメソッドを送って外部リンクのデータをDBに保存

--- client/src/app/(app)/mypage/linkedit/_component/LinkForm.js
+++ client/src/app/(app)/mypage/linkedit/_component/LinkForm.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Label } from "@@/(app)/mypage/linkedit/_component/Label"
 import { Input } from "@@/(app)/mypage/linkedit/_component/Input"
 import { Button } from "@@/(app)/mypage/linkedit/_component/Button"
+import laravelAxios from '@/lib/laravelAxios';
+
 const LinkForm = () => {
+    const [url, setUrl] = useState('');
+    const [site_name, setName] = useState('');
+
+    const handleSubmit = async (e) => {
+        console.log(e)
+        e.preventDefault();
+        console.log("URL:", url); // デバッグ用
+        console.log("Name:", site_name); // デバッグ用
+        const response = await laravelAxios.post('http://localhost:8080/api/mypage/externallinks/create', {
+            url,
+            site_name
+        }, {
+            withCredentials: true
+        });
+        console.log("Response:", response); // デバッグ用
+    } 
+
     return (
-        <form className="space-y-4 col-span-1 mr-3.5">
+        <form className="space-y-4 col-span-1 mr-3.5" onSubmit={handleSubmit}>
             <div className="grid gap-4">
                 <div>
                     <Label htmlFor="url" className="">
@@ -18,11 +37,11 @@ const LinkForm = () => {
                         </span>
                     </Label>
 
-                    <Input id="url" type="url" placeholder="(例) https://kabutan.jp/stock/finance?code=[code]" className="w-full placeholder-gray-200" />
+                    <Input id="url" type="url" placeholder="(例) https://kabutan.jp/stock/finance?code=[code]" className="w-full placeholder-gray-200" value={url} onChange={(e) => setUrl(e.target.value)} />
                 </div>
                 <div>
                     <Label htmlFor="name">Name</Label>
-                    <Input id="name" type="text" placeholder="(例) 株探" className="w-full placeholder-gray-200" />
+                    <Input id="name" type="text" placeholder="(例) 株探" className="w-full placeholder-gray-200" value={site_name} onChange={(e) => setName(e.target.value)} />
                 </div>
             </div>
             <Button type="submit" className="w-full">
