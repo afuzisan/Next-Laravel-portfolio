221366769fb81bfae51a6ae4a85afa5edd8037d2
akiyaoka <akiyaoka@gmail.com>
2024-08-27 12:54:17 +0900

    本番環境用のファイルを色々修正

--- server/docker-entrypoint.prod.sh
+++ server/docker-entrypoint.prod.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
-# ストレージリンク作成
-php artisan storage:link
 
 php artisan config:cache
 
+# 必要なディレクトリが存在しない場合は作成
+mkdir -p /var/www/client/storage/
 
+# 必要なディレクトリの権限を設定
+chmod -R 777 /var/www/client/storage/
+
+# Composerの依存関係をインストール
+composer install --no-dev --optimize-autoloader
 
 # Apache起動
 exec apache2-foreground
